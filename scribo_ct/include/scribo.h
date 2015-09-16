02f7b5d67cbe46064aaf24a463ec714344fb611a
Petr Vepřek <petr.veprek.cz420@gmail.com>
2015-09-16 21:41:17 +0200

    Made implementation conditional

--- scribo_ct/include/scribo.h
+++ scribo_ct/include/scribo.h
@@ -12,207 +12,210 @@
 #   error "Macro SCRIBO is already defined!"
 #endif
 
-// Interpret configuration shorthand
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_FATAL
-#       undef SCRIBO_DISABLE_VERBOSITY_FATAL
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_FATAL 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_ERROR
-#       undef SCRIBO_DISABLE_VERBOSITY_ERROR
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_ERROR 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_WARNING
-#       undef SCRIBO_DISABLE_VERBOSITY_WARNING
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_WARNING 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_LOG
-#       undef SCRIBO_DISABLE_VERBOSITY_LOG
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_LOG 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_INFO
-#       undef SCRIBO_DISABLE_VERBOSITY_INFO
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_INFO 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_DEBUG
-#       undef SCRIBO_DISABLE_VERBOSITY_DEBUG
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_DEBUG 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_METHOD_ETC  == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_METHOD
-#       undef SCRIBO_DISABLE_VERBOSITY_METHOD
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_METHOD 1
-#endif
-#if          (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_METHOD_ETC  == 1) || \
-             (SCRIBO_DISABLE_VERBOSITY_TRACE_ETC   == 1)
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_TRACE
-#       undef SCRIBO_DISABLE_VERBOSITY_TRACE
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_TRACE 1
-#endif
-
-// Interpret usage shorthand
-#define SCRIBOF(...) SCRIBO(FATAL,   __VA_ARGS__)
-#define SCRIBOE(...) SCRIBO(ERROR,   __VA_ARGS__)
-#define SCRIBOW(...) SCRIBO(WARNING, __VA_ARGS__)
-#define SCRIBOL(...) SCRIBO(LOG,     __VA_ARGS__)
-#define SCRIBOI(...) SCRIBO(INFO,    __VA_ARGS__)
-#define SCRIBOD(...) SCRIBO(DEBUG,   __VA_ARGS__)
-#define SCRIBOM(...) SCRIBO(METHOD,  __VA_ARGS__)
-#define SCRIBOT(...) SCRIBO(TRACE,   __VA_ARGS__)
-
-// Initialize default category
-#ifndef    SCRIBO_CATEGORY
-#	define SCRIBO_CATEGORY GENERIC
-#endif
-
-// Configure disabling current category
-#define SCRIBO__GET_CATEGORY_DISABLED_LOOKUP(CATEGORY) SCRIBO_DISABLE_CATEGORY_ ## CATEGORY
-#define SCRIBO__GET_CATEGORY_DISABLED(       CATEGORY) SCRIBO__GET_CATEGORY_DISABLED_LOOKUP(CATEGORY)
-#if SCRIBO__GET_CATEGORY_DISABLED(SCRIBO_CATEGORY) == 1
-#	define SCRIBO__DISABLE_CATEGORY 1
-#else
-#	define SCRIBO__DISABLE_CATEGORY 0
-#endif
-
-// Configure disabling known verbosities
-#if           SCRIBO_DISABLE_VERBOSITY_FATAL != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_FATAL
-#       undef SCRIBO_DISABLE_VERBOSITY_FATAL
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_FATAL 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_ERROR != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_ERROR
-#       undef SCRIBO_DISABLE_VERBOSITY_ERROR
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_ERROR 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_WARNING != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_WARNING
-#       undef SCRIBO_DISABLE_VERBOSITY_WARNING
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_WARNING 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_LOG != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_LOG
-#       undef SCRIBO_DISABLE_VERBOSITY_LOG
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_LOG 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_INFO != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_INFO
-#       undef SCRIBO_DISABLE_VERBOSITY_INFO
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_INFO 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_DEBUG != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_DEBUG
-#       undef SCRIBO_DISABLE_VERBOSITY_DEBUG
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_DEBUG 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_METHOD != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_METHOD
-#       undef SCRIBO_DISABLE_VERBOSITY_METHOD
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_METHOD 0
-#endif
-#if           SCRIBO_DISABLE_VERBOSITY_TRACE != 1
-#   ifdef     SCRIBO_DISABLE_VERBOSITY_TRACE
-#       undef SCRIBO_DISABLE_VERBOSITY_TRACE
-#   endif
-#   define    SCRIBO_DISABLE_VERBOSITY_TRACE 0
-#endif
-
-// Configure enabling combinations of current category & known verbosities
-#define SCRIBO__GET_COMBO_ENABLED_LOOKUP(CATEGORY, VERBOSITY) SCRIBO_ENABLE_CATEGORY_ ## CATEGORY ## _ ## VERBOSITY
-#define SCRIBO__GET_COMBO_ENABLED(       CATEGORY, VERBOSITY) SCRIBO__GET_COMBO_ENABLED_LOOKUP(CATEGORY, VERBOSITY_ ## VERBOSITY)
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, FATAL) == 1
-#   define SCRIBO__ENABLE_COMBO_FATAL 1
-#else
-#   define SCRIBO__ENABLE_COMBO_FATAL 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, ERROR) == 1
-#   define SCRIBO__ENABLE_COMBO_ERROR 1
-#else
-#   define SCRIBO__ENABLE_COMBO_ERROR 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, WARNING) == 1
-#   define SCRIBO__ENABLE_COMBO_WARNING 1
-#else
-#   define SCRIBO__ENABLE_COMBO_WARNING 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, LOG) == 1
-#   define SCRIBO__ENABLE_COMBO_LOG 1
-#else
-#   define SCRIBO__ENABLE_COMBO_LOG 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, INFO) == 1
-#   define SCRIBO__ENABLE_COMBO_INFO 1
-#else
-#   define SCRIBO__ENABLE_COMBO_INFO 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, DEBUG) == 1
-#   define SCRIBO__ENABLE_COMBO_DEBUG 1
-#else
-#   define SCRIBO__ENABLE_COMBO_DEBUG 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, METHOD) == 1
-#   define SCRIBO__ENABLE_COMBO_METHOD 1
-#else
-#   define SCRIBO__ENABLE_COMBO_METHOD 0
-#endif
-#if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, TRACE) == 1
-#   define SCRIBO__ENABLE_COMBO_TRACE 1
-#else
-#   define SCRIBO__ENABLE_COMBO_TRACE 0
-#endif
-
-// Process scribo logging
+// To scribo or not to scribo
 #if SCRIBO_DISABLE_ALL == 1
     // Remove scribo when disabled
 #   define SCRIBO(...)
 #else
+    
+    // Interpret configuration shorthand
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_FATAL
+#           undef SCRIBO_DISABLE_VERBOSITY_FATAL
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_FATAL 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_ERROR
+#           undef SCRIBO_DISABLE_VERBOSITY_ERROR
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_ERROR 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_WARNING
+#           undef SCRIBO_DISABLE_VERBOSITY_WARNING
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_WARNING 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_LOG
+#           undef SCRIBO_DISABLE_VERBOSITY_LOG
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_LOG 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_INFO
+#           undef SCRIBO_DISABLE_VERBOSITY_INFO
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_INFO 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_DEBUG
+#           undef SCRIBO_DISABLE_VERBOSITY_DEBUG
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_DEBUG 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_METHOD_ETC  == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_METHOD
+#           undef SCRIBO_DISABLE_VERBOSITY_METHOD
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_METHOD 1
+#   endif
+#   if           (SCRIBO_DISABLE_VERBOSITY_FATAL_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_ERROR_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_WARNING_ETC == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_LOG_ETC     == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_INFO_ETC    == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_DEBUG_ETC   == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_METHOD_ETC  == 1) || \
+                 (SCRIBO_DISABLE_VERBOSITY_TRACE_ETC   == 1)
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_TRACE
+#           undef SCRIBO_DISABLE_VERBOSITY_TRACE
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_TRACE 1
+#   endif
+    
+    // Interpret usage shorthand
+#   define SCRIBOF(...) SCRIBO(FATAL,   __VA_ARGS__)
+#   define SCRIBOE(...) SCRIBO(ERROR,   __VA_ARGS__)
+#   define SCRIBOW(...) SCRIBO(WARNING, __VA_ARGS__)
+#   define SCRIBOL(...) SCRIBO(LOG,     __VA_ARGS__)
+#   define SCRIBOI(...) SCRIBO(INFO,    __VA_ARGS__)
+#   define SCRIBOD(...) SCRIBO(DEBUG,   __VA_ARGS__)
+#   define SCRIBOM(...) SCRIBO(METHOD,  __VA_ARGS__)
+#   define SCRIBOT(...) SCRIBO(TRACE,   __VA_ARGS__)
+    
+    // Define general helpers
+#   define SCRIBO__PASTE_2(_1, _2)     _1 ## _2
+#   define SCRIBO__PASTE_3(_1, _2, _3) _1 ## _2 ## _3
+    
+    // Initialize default category
+#   ifndef     SCRIBO_CATEGORY
+#       define SCRIBO_CATEGORY GENERIC
+#   endif
+    
+    // Configure disabling current category
+#   define SCRIBO__GET_CATEGORY_DISABLED(CATEGORY) SCRIBO__PASTE_2(SCRIBO_DISABLE_CATEGORY_, CATEGORY)
+#   if SCRIBO__GET_CATEGORY_DISABLED(SCRIBO_CATEGORY) == 1
+#       define SCRIBO__DISABLE_CATEGORY 1
+#   else
+#       define SCRIBO__DISABLE_CATEGORY 0
+#   endif
+    
+    // Configure disabling known verbosities
+#   if            SCRIBO_DISABLE_VERBOSITY_FATAL != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_FATAL
+#           undef SCRIBO_DISABLE_VERBOSITY_FATAL
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_FATAL 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_ERROR != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_ERROR
+#           undef SCRIBO_DISABLE_VERBOSITY_ERROR
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_ERROR 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_WARNING != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_WARNING
+#           undef SCRIBO_DISABLE_VERBOSITY_WARNING
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_WARNING 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_LOG != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_LOG
+#           undef SCRIBO_DISABLE_VERBOSITY_LOG
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_LOG 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_INFO != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_INFO
+#           undef SCRIBO_DISABLE_VERBOSITY_INFO
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_INFO 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_DEBUG != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_DEBUG
+#           undef SCRIBO_DISABLE_VERBOSITY_DEBUG
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_DEBUG 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_METHOD != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_METHOD
+#           undef SCRIBO_DISABLE_VERBOSITY_METHOD
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_METHOD 0
+#   endif
+#   if            SCRIBO_DISABLE_VERBOSITY_TRACE != 1
+#       ifdef     SCRIBO_DISABLE_VERBOSITY_TRACE
+#           undef SCRIBO_DISABLE_VERBOSITY_TRACE
+#       endif
+#       define    SCRIBO_DISABLE_VERBOSITY_TRACE 0
+#   endif
+    
+    // Configure enabling combinations of current category & known verbosities
+#   define SCRIBO__GET_COMBO_ENABLED(CATEGORY, VERBOSITY) SCRIBO__PASTE_3(SCRIBO_ENABLE_CATEGORY_, CATEGORY, VERBOSITY_ ## VERBOSITY)
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, FATAL) == 1
+#       define SCRIBO__ENABLE_COMBO_FATAL 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_FATAL 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, ERROR) == 1
+#       define SCRIBO__ENABLE_COMBO_ERROR 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_ERROR 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, WARNING) == 1
+#       define SCRIBO__ENABLE_COMBO_WARNING 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_WARNING 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, LOG) == 1
+#       define SCRIBO__ENABLE_COMBO_LOG 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_LOG 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, INFO) == 1
+#       define SCRIBO__ENABLE_COMBO_INFO 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_INFO 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, DEBUG) == 1
+#       define SCRIBO__ENABLE_COMBO_DEBUG 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_DEBUG 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, METHOD) == 1
+#       define SCRIBO__ENABLE_COMBO_METHOD 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_METHOD 0
+#   endif
+#   if SCRIBO__GET_COMBO_ENABLED(SCRIBO_CATEGORY, TRACE) == 1
+#       define SCRIBO__ENABLE_COMBO_TRACE 1
+#   else
+#       define SCRIBO__ENABLE_COMBO_TRACE 0
+#   endif
+    
     // Prepare scribo parameters
 #   define SCRIBO__EXPAND_ARGUMENTS(ARGUMENTS) ARGUMENTS
 #   define SCRIBO__PICK_23RD(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, ...) _23
@@ -323,6 +326,7 @@
             SCRIBO__DO_FLUSH; \
             SCRIBO__INCREMENT_COUNTER; \
         } while (0)
+    
 #endif
 
 // End of file ---------------------------------------------------------------------------------------------------------
